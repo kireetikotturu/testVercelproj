b321abc9461b0b56a3ee18341c8995ecbab3e49f
kireetikotturu <kireetikotturi2@gmail.com>
2026-02-10 19:36:08 +0530

    first commit

---
+++ Desktop/NewReactProj/reactMaterialUI/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import CourseCard from './components/Card'
+const App = () => {
+  return (
+
+    <div>
+      <Header/>
+      <CourseCard/>
+    </div>
+  )
+}
+
+export default App
